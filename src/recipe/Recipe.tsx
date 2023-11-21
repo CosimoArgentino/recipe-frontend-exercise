97a5422c4a31c36358cc9526857d9839bf4603f0
Cosimo Argentino <cosimoargentino@gmail.com>
2023-11-21 17:44:04 +0100

    elastic ui added
    elastic ui added

---
+++ src/recipe/Recipe.tsx
@@ -0,0 +1,10 @@
+import { EuiAbsoluteTab } from "@elastic/eui";
+import { useState } from "react";
+
+export default function Recipe() {
+    return (
+        <>
+            <EuiAbsoluteTab dateFormat="" timeFormat="" value="" onChange={()=> {}} roundUp position="start" labelPrefix=""></EuiAbsoluteTab>
+        </>
+    )
+}
